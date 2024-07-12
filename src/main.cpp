c0710000de32c83d96899c68e4c26ef8402a3ce8
gavin lou <gavinlouuu@gmail.com>
2024-07-12 17:04:16 +0800

    clean up structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <Wire.h>
-// #include <FS.h>
 #include <pinConfig.h>
 #include <Init.h>
 #include <FirebaseJson.h>
@@ -12,15 +11,10 @@
 #include <Hardware.h>
 #include <beagleCLI.h>
 #include <Network.h>
-// #include <SPI.h>
 #include <Firebase_ESP_Client.h>
 #include <M5Stack.h>
 #include <exp_setup.h>
 #include <zsrelay.h>
-
-// #include <SensorData.h>
-// #include <SensorDataFactory.h>
-// #include <SensorDataFactory.cpp>
 
 void setup()
 {
@@ -29,32 +23,17 @@ void setup()
   Serial2.begin(38400, SERIAL_8N1, U2_RX, U2_TX);
   M5.begin(1, 1, 1, 1);
   Wire.setClock(400000); // Increase I2C clock speed to 400 kHz
-  //  the setups are not not needed now
-  pinSetup(); // something in pin setup is causing sd card to not initialize properly
-  // delay(100);
 
-  pwmSetup(); // something in pwm setup is causing sd card to not initialize properly
-  // delay(100);
-  // ledcWrite(PWM_H_CH, 150);
-
+  pinSetup();
+  pwmSetup();
   relay_off();
-
   cmdSetup();
   if (!SD.exists("/wifiCredentials.json"))
   {
     Serial.println("wifiCredentials.json does not exist");
     return;
   }
-
   networkCheck();
-
-  // sampleTask();
-  // M5_example();
-  // const char *filename = "/expSetup.json";
-  // String configData;
-  // M5_SD_JSON(filename, configData);
-
-  // sensorCheck();
 }
 
 void loop()
