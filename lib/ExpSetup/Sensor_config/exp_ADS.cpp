ff3562b9c00bc97d550a37f0d8e513642a4781f1
gavin lou <gavinlouuu@gmail.com>
2024-07-16 19:25:42 +0800

    commentout previous code

--- lib/ExpSetup/Sensor_config/exp_ADS.cpp
+++ lib/ExpSetup/Sensor_config/exp_ADS.cpp
@@ -140,131 +140,131 @@ void saveADSDataFromBuffer(const CircularBuffer<SettingData, bufferSize> &buffer
     Serial.println("Data dumped to: " + filename);
 }
 
-// previous working code
-constexpr size_t bufferSize = 5000;
+// // previous working code
+// constexpr size_t bufferSize = 5000;
 
-struct SettingData
-{
-    int setting;
-    unsigned long timestamp;
-    int16_t value;
-};
+// struct SettingData
+// {
+//     int setting;
+//     unsigned long timestamp;
+//     int16_t value;
+// };
 
-CircularBuffer<SettingData, bufferSize> ADSBuffer;
+// CircularBuffer<SettingData, bufferSize> ADSBuffer;
 
-void adsFastSampleTask(TaskHandle_t *taskHandle)
-{
-    xTaskCreate(
-        sampleADScontinuous,    // Function that the task will run
-        "ADS Fast Sample Task", // Name of the task
-        20240,                  // Stack size
-        NULL,                   // Parameters to pass (can be modified as needed)
-        1,                      // Task priority
-        taskHandle              // Pointer to handle
-    );
-}
+// void adsFastSampleTask(TaskHandle_t *taskHandle)
+// {
+//     xTaskCreate(
+//         sampleADScontinuous,    // Function that the task will run
+//         "ADS Fast Sample Task", // Name of the task
+//         20240,                  // Stack size
+//         NULL,                   // Parameters to pass (can be modified as needed)
+//         1,                      // Task priority
+//         taskHandle              // Pointer to handle
+//     );
+// }
 
-void sampleADScontinuous(void *pvParameters)
-{
-    const unsigned long saveInterval = 300000; // 5 minutes in milliseconds
-    unsigned long lastSaveTime = millis();
+// void sampleADScontinuous(void *pvParameters)
+// {
+//     const unsigned long saveInterval = 300000; // 5 minutes in milliseconds
+//     unsigned long lastSaveTime = millis();
 
-    for (;;)
-    {
-        // Wait for a notification to start data acquisition
-        Serial.println("RUNNER: Waiting for notification to start data acquisition.");
-        ulTaskNotifyTake(pdTRUE, portMAX_DELAY); // Start of a new experiment
-        Serial.println("RUNNER: Start data acquisition.");
-        ADSBuffer.clear();
-        String filename = setupSave(setup_tracker, repeat_tracker, channel_tracker, exp_name);
+//     for (;;)
+//     {
+//         // Wait for a notification to start data acquisition
+//         Serial.println("RUNNER: Waiting for notification to start data acquisition.");
+//         ulTaskNotifyTake(pdTRUE, portMAX_DELAY); // Start of a new experiment
+//         Serial.println("RUNNER: Start data acquisition.");
+//         ADSBuffer.clear();
+//         String filename = setupSave(setup_tracker, repeat_tracker, channel_tracker, exp_name);
 
-        while (true)
-        {
-            // Perform data acquisition
-            // std::array<int16_t, 4> results;  // Simulate results
-            // int currentSetting = getCurrentSetting();  // Get the current setting
-            UOM_ADS_continuous(ADSBuffer, heaterSettings, heatingTime);
-            Serial.print("*");
+//         while (true)
+//         {
+//             // Perform data acquisition
+//             // std::array<int16_t, 4> results;  // Simulate results
+//             // int currentSetting = getCurrentSetting();  // Get the current setting
+//             UOM_ADS_continuous(ADSBuffer, heaterSettings, heatingTime);
+//             Serial.print("*");
 
-            // Check if it's time to save data
-            if (millis() - lastSaveTime >= saveInterval || ADSBuffer.isFull())
-            {
+//             // Check if it's time to save data
+//             if (millis() - lastSaveTime >= saveInterval || ADSBuffer.isFull())
+//             {
 
-                saveADSDataFromBuffer(ADSBuffer, filename);
-                lastSaveTime = millis(); // Reset the timer after saving
-                ADSBuffer.clear();       // Optionally clear buffer after saving if appropriate
-            }
+//                 saveADSDataFromBuffer(ADSBuffer, filename);
+//                 lastSaveTime = millis(); // Reset the timer after saving
+//                 ADSBuffer.clear();       // Optionally clear buffer after saving if appropriate
+//             }
 
-            // Check for a stop notification without waiting
-            if (ulTaskNotifyTake(pdTRUE, 0))
-            {
-                Serial.println("RUNNER: Data saving notification received.");
-                break; // Stop data acquisition on notification
-            }
-        }
+//             // Check for a stop notification without waiting
+//             if (ulTaskNotifyTake(pdTRUE, 0))
+//             {
+//                 Serial.println("RUNNER: Data saving notification received.");
+//                 break; // Stop data acquisition on notification
+//             }
+//         }
 
-        // Optionally perform a final save if there's data left in the buffer
-        if (!ADSBuffer.isEmpty())
-        {
-            // String filename = setupSave(setup_tracker, repeat_tracker, channel_tracker, exp_name);
-            saveADSDataFromBuffer(ADSBuffer, filename);
-            ADSBuffer.clear(); // Clear buffer after final saving
-        }
+//         // Optionally perform a final save if there's data left in the buffer
+//         if (!ADSBuffer.isEmpty())
+//         {
+//             // String filename = setupSave(setup_tracker, repeat_tracker, channel_tracker, exp_name);
+//             saveADSDataFromBuffer(ADSBuffer, filename);
+//             ADSBuffer.clear(); // Clear buffer after final saving
+//         }
 
-        // Notify that this round of data collection is complete
-        Serial.println("RUNNER: Data saving complete. Notifying expLoopTask.");
-        xTaskNotifyGive(expLoopTaskHandle);
-        // Block until the next run or termination
+//         // Notify that this round of data collection is complete
+//         Serial.println("RUNNER: Data saving complete. Notifying expLoopTask.");
+//         xTaskNotifyGive(expLoopTaskHandle);
+//         // Block until the next run or termination
 
-        // Serial.println("RUNNER: Waiting for next experiment or termination.");
-        // ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-        // Serial.println("RUNNER: Received notification to start next experiment.");
-    }
-}
+//         // Serial.println("RUNNER: Waiting for next experiment or termination.");
+//         // ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+//         // Serial.println("RUNNER: Received notification to start next experiment.");
+//     }
+// }
 
-void UOM_ADS_continuous(CircularBuffer<SettingData, bufferSize> &buffer, const std::vector<int> &heaterSettings, int heatingTime)
-{
-    for (int setting : heaterSettings)
-    {
-        ledcWrite(PWM_Heater, setting);
-        unsigned long timestamp = millis();              // Get current timestamp
-        int16_t result = ads.getLastConversionResults(); // Get sensor reading
+// void UOM_ADS_continuous(CircularBuffer<SettingData, bufferSize> &buffer, const std::vector<int> &heaterSettings, int heatingTime)
+// {
+//     for (int setting : heaterSettings)
+//     {
+//         ledcWrite(PWM_Heater, setting);
+//         unsigned long timestamp = millis();              // Get current timestamp
+//         int16_t result = ads.getLastConversionResults(); // Get sensor reading
 
-        SettingData data;           // Create a struct instance
-        data.setting = setting;     // Assign the setting
-        data.timestamp = timestamp; // Assign the timestamp
-        data.value = result;        // Assign the sensor result
+//         SettingData data;           // Create a struct instance
+//         data.setting = setting;     // Assign the setting
+//         data.timestamp = timestamp; // Assign the timestamp
+//         data.value = result;        // Assign the sensor result
 
-        buffer.push(data); // Push structured data to circular buffer
-    }
-}
+//         buffer.push(data); // Push structured data to circular buffer
+//     }
+// }
 
-String continuousADS_Header = "Setting,Timestamp,Value";
-void saveADSDataFromBuffer(const CircularBuffer<SettingData, bufferSize> &buffer, const String &filename)
-{
-    File myFile = SD.open(filename, FILE_APPEND); // Change here to FILE_APPEND
-    if (!myFile)
-    {
-        Serial.println("Error opening file for writing");
-        return;
-    }
+// String continuousADS_Header = "Setting,Timestamp,Value";
+// void saveADSDataFromBuffer(const CircularBuffer<SettingData, bufferSize> &buffer, const String &filename)
+// {
+//     File myFile = SD.open(filename, FILE_APPEND); // Change here to FILE_APPEND
+//     if (!myFile)
+//     {
+//         Serial.println("Error opening file for writing");
+//         return;
+//     }
 
-    // The header should only be written if the file was newly created or is empty
-    if (myFile.size() == 0)
-    {
-        myFile.println(continuousADS_Header); // Write header only if file is empty
-    }
+//     // The header should only be written if the file was newly created or is empty
+//     if (myFile.size() == 0)
+//     {
+//         myFile.println(continuousADS_Header); // Write header only if file is empty
+//     }
 
-    // Dump all buffer data to file
-    for (size_t i = 0; i < buffer.size(); i++)
-    {
-        const SettingData &data = buffer[i];
-        myFile.printf("%d,%lu,%d\n", data.setting, data.timestamp, data.value);
-    }
+//     // Dump all buffer data to file
+//     for (size_t i = 0; i < buffer.size(); i++)
+//     {
+//         const SettingData &data = buffer[i];
+//         myFile.printf("%d,%lu,%d\n", data.setting, data.timestamp, data.value);
+//     }
 
-    myFile.close();
-    Serial.println("Data dumped to: " + filename);
-}
+//     myFile.close();
+//     Serial.println("Data dumped to: " + filename);
+// }
 
 //-----------------------------------------------------------------------------------------------
 
